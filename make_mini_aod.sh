592b1145ac95056a4d9634c76666c25489b91cbf
Andrew <anlevin@cern.ch>
2015-08-09 16:28:48 +0200

    no commit message

--- make_mini_aod.sh
+++ make_mini_aod.sh
@@ -33,104 +33,49 @@ echo $lumi_number
 echo \$hadron_fragment
 echo $hadron_fragment
 
-#cd /afs/cern.ch/work/a/anlevin/mc_production/CMSSW_6_2_0_patch1/src/;
-#cd /afs/cern.ch/work/a/anlevin/mc_production/CMSSW_6_2_4/src;
-#cd /afs/cern.ch/work/a/anlevin/mc_production/CMSSW_7_2_0/src/
-cd /afs/cern.ch/work/a/anlevin/mc_production/CMSSW_7_1_14/src/
+cd /afs/cern.ch/work/a/anlevin/mc_production/CMSSW_7_1_15_patch1/src/
 eval `scramv1 runtime -sh`;
 cd -; 
 
 echo "begin step 1"
 
-#cmsDriver.py step1 --filein file:$input_file --mc --eventcontent LHE --datatier GEN --conditions START62_V1::All --step NONE --python_filename step1.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n $n_events --customise_commands process.source.skipEvents\ =\ cms.untracked.uint32\($where_to_start\)\\nprocess.source.firstLuminosityBlock\ =\ cms.untracked.uint32\($lumi_number\) --fileout file:step1_output.root
-cmsDriver.py step1 --filein file:$input_file --mc --eventcontent LHE --datatier GEN --conditions PHYS14_25_V1  --step NONE --python_filename step1.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n $n_events --customise_commands process.source.skipEvents\ =\ cms.untracked.uint32\($where_to_start\)\\nprocess.source.firstLuminosityBlock\ =\ cms.untracked.uint32\($lumi_number\) --fileout file:step1_output.root
+cmsDriver.py $hadron_fragment --fileout file:step1_output.root --mc --eventcontent RAWSIM --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1,Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --inputCommands 'keep *','drop LHEXMLStringProduct_*_*_*' --conditions MCRUN2_71_V1::All --beamspot NominalCollision2015 --step GEN,SIM --magField 38T_PostLS1 --filein file:$input_file -n $n_events --customise_commands process.source.skipEvents\ =\ cms.untracked.uint32\($where_to_start\)\\nprocess.source.firstLuminosityBlock\ =\ cms.untracked.uint32\($lumi_number\);
 
-cmsRun step1.py
+echo "finished step 1"
 
-echo "end step 1"
-
-echo "ls -lh"
-ls -lh
-
-cmsDriver.py Configuration/GenProduction/python/ThirteenTeV/MinBias_Tune4C_13TeV_pythia8_cff --mc --eventcontent RAWSIM --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1 --datatier GEN-SIM --conditions PHYS14_25_V1 --step GEN,SIM --magField 38T_PostLS1 --geometry Extended2015 --python_filename minbias.py --no_exec -n $n_events_pileup --customise Configuration/GenProduction/randomizeSeeds.randomizeSeeds --fileout file:pileup.root
-
-cmsRun minbias.py
-
-cd /afs/cern.ch/work/a/anlevin/mc_production/CMSSW_7_2_0/src/
-#cd /afs/cern.ch/work/a/anlevin/mc_production/CMSSW_6_2_4/src;
+cd /afs/cern.ch/work/a/anlevin/mc_production/CMSSW_7_1_13/src/
 eval `scramv1 runtime -sh`;
-cd -;
+cd -; 
+
+echo "begin pileup step"
+
+cmsDriver.py Configuration/GenProduction/python/FSQ-RunIIWinter15GS-00001-fragment.py --fileout file:pileup.root --mc --eventcontent RAWSIM --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1,Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --conditions MCRUN2_71_V1::All --beamspot NominalCollision2015 --step GEN,SIM --magField 38T_PostLS1 -n $n_events_pileup --customise Configuration/GenProduction/randomizeSeeds.randomizeSeeds
+
+echo "end pileup step"
+
+cd /afs/cern.ch/work/a/anlevin/mc_production/CMSSW_7_4_1_patch1/src/
+eval `scramv1 runtime -sh`;
+cd -; 
 
 echo "begin step 2"
 
-cmsDriver.py $hadron_fragment --filein file:step1_output.root --fileout file:step2_output.root --mc --eventcontent RAWSIM --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1,Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM --conditions PHYS14_25_V1 --step GEN,SIM --magField 38T_PostLS1 --geometry Extended2015 --python_filename step2.py --no_exec -n -1 --customise Configuration/GenProduction/randomizeSeeds.randomizeSeeds
+cmsDriver.py step2 --filein file:step1_output.root --fileout file:step2_output.root --pileup_input file:pileup.root --mc --eventcontent RAWSIM --pileup 2015_25ns_Startup_PoissonOOTPU --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1,Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM-RAW --conditions MCRUN2_74_V9 --step DIGI,L1,DIGI2RAW,HLT:@frozen25ns --magField 38T_PostLS1 -n -1
 
-cmsRun step2.py
-
-echo "end step 2"
-
-echo "ls -lh"
-ls -lh
-
-cd /afs/cern.ch/work/a/anlevin/mc_production/CMSSW_7_2_0/src/
-eval `scramv1 runtime -sh`;
-cd -;
+echo "finished step 2"
 
 echo "begin step 3"
 
-cmsDriver.py step1 --filein file:step2_output.root --fileout file:step3_output.root --pileup_input file:pileup.root --mc --eventcontent RAWSIM --inputEventContent REGEN --pileup Phys14_50ns_PoissonOOT --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1,Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM-RAW --conditions PHYS14_25_V1 --step GEN:fixGenInfo,DIGI,L1,DIGI2RAW,HLT:GRun --magField 38T_PostLS1 --python_filename step3.py --no_exec -n -1
+cmsDriver.py step3 --filein file:step2_output.root --fileout file:step3_output.root --mc --eventcontent AODSIM,DQM --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1,Configuration/DataProcessing/Utils.addMonitoring --datatier AODSIM,DQMIO --conditions MCRUN2_74_V9 --step RAW2DIGI,L1Reco,RECO,EI,DQM:DQMOfflinePOGMC --magField 38T_PostLS1 -n -1 
 
-#cmsDriver.py step3 --filein file:step2_output.root --fileout file:step3_output.root --pileup_input file:pileup.root --mc --eventcontent RAWSIM --pileup AVE_20_BX_25ns --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1,Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM-RAW --conditions POSTLS170_V5::All --step DIGI,L1,DIGI2RAW,HLT:2013,RAW2DIGI,L1Reco --magField 38T_PostLS1 --geometry DBExtendedPostLS1 --python_filename step3.py --no_exec -n -1 --customise Configuration/GenProduction/randomizeSeeds.randomizeSeeds
-
-cmsRun step3.py
-
-echo "end step 3"
-
-
-echo "ls -lh"
-ls -lh
+echo "finished step 3"
 
 echo "begin step 4"
 
-cmsDriver.py step4 --filein file:step3_output.root --fileout file:step4_output.root --mc --eventcontent AODSIM --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1,Configuration/DataProcessing/Utils.addMonitoring --datatier AODSIM --conditions PHYS14_25_V1 --step RAW2DIGI,L1Reco,RECO,EI --magField 38T_PostLS1 --python_filename step4.py --no_exec -n -1
+cmsDriver.py step4 --filein file:step3_output.root --fileout file:step4_output.root --mc --eventcontent MINIAODSIM --runUnscheduled --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1,Configuration/DataProcessing/Utils.addMonitoring --datatier MINIAODSIM --conditions MCRUN2_74_V9 --step PAT -n -1
 
-#cmsDriver.py --filein file:step3_output.root --fileout file:step4_output.root --mc --eventcontent AODSIM --customise SLHCUpgradeSimulations/Configuration/postLS1Customs.customisePostLS1,Configuration/DataProcessing/Utils.addMonitoring --datatier AODSIM --conditions POSTLS170_V5::All --step RAW2DIGI,L1Reco,RECO,EI --magField 38T_PostLS1 --geometry DBExtendedPostLS1 --python_filename step4.py --no_exec -n -1 --customise Configuration/GenProduction/randomizeSeeds.randomizeSeeds
+echo "finished step 4"
 
-cmsRun step4.py
-
-echo "end step 4"
-
-echo "ls -lh"
-ls -lh
-
-#cd /afs/cern.ch/work/a/anlevin/mc_production/CMSSW_7_0_6_patch1/src/;
-#eval `scramv1 runtime -sh`;
-#cd -;
-
-echo "begin step 5"
-
-cmsDriver.py step3 --filein file:step4_output.root --fileout file:step5_output.root --mc --eventcontent MINIAODSIM --runUnscheduled --datatier MINIAODSIM --conditions PHYS14_25_V1 --step PAT --python_filename step5.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n -1
-
-#cmsDriver.py step5 --filein file:step4_output.root --fileout file:step5_output.root --mc --eventcontent MINIAODSIM --runUnscheduled --datatier MINIAODSIM --conditions PLS170_V7AN1::All --step PAT --python_filename step5.py --no_exec --customise Configuration/DataProcessing/Utils.addMonitoring -n -1 --customise Configuration/GenProduction/randomizeSeeds.randomizeSeeds
-
-cmsRun step5.py
-
-echo "end step 5"
-
-echo "ls -lh"
-ls -lh
-
-echo "dumping configuration files"
-echo ""
-
-for file in `ls | grep "\.py" | grep -v pyc`; do echo "begin dumping file $file"; echo "";  cat $file; echo ""; echo "finished dumping file $file"; done
-
-if ! cat *py | grep  Pythia8HadronizerFilter >& /dev/null; then
-    echo "no hadronizer in the configuration files, exiting"
-    exit
-fi
-
-/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select cp step5_output.root ${output_dir}step5_output_$where_to_start.root
+/afs/cern.ch/project/eos/installation/0.3.84-aquamarine/bin/eos.select cp step4_output.root ${output_dir}step4_output_$where_to_start.root
 
 echo date
 date
